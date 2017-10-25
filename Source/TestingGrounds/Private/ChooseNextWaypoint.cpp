f7817d907d7292c82fbbf544b4ade4077c2b47d4
TWOGUYS\Jimmy2Guys <jimmytwoguys@gmail.com>
2017-10-25 14:02:36 -0400

    218. Reading Blackboard Data in C++

--- Source/TestingGrounds/Private/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/Private/ChooseNextWaypoint.cpp
@@ -1,10 +1,14 @@
 // Copyright (C) 2017 JimmyTwoGuys.  All Rights Reserved.
 
 #include "ChooseNextWaypoint.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-	UE_LOG(LogTemp, Warning, TEXT("AI in C++ YEA!!!! donkey"));
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+
+	UE_LOG(LogTemp, Warning, TEXT("Waypoint Index:  %i donkey"));
 	return EBTNodeResult::Succeeded;
 }
 
